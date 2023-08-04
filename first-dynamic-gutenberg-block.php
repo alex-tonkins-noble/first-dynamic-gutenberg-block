b34c67c23f10a0de24698ffa527cab002602e9d3
Alex Tonkins <alex.tonkins@nobleperforms.co.uk>
2023-08-04 17:04:33 +0100

    update block to render php callback in the save state

--- first-dynamic-gutenberg-block.php
+++ first-dynamic-gutenberg-block.php
@@ -21,8 +21,66 @@
  *
  * @see https://developer.wordpress.org/reference/functions/register_block_type/
  */
-function create_first_dynamic_gutenberg_block_block_init()
-{
-	register_block_type(__DIR__ . '/build');
+
+function blocks_course_render_frontend($attributes) {
+	$display_image = $attributes['displayFeaturedImage'];
+
+	$query_args = [
+		'posts_per_page' => $attributes['numberOfPosts'],
+		'post_status' => 'publish'
+	];
+
+	$query = new WP_Query($query_args);
+	$posts = $query->posts;
+
+	// print_r(($posts));
+
+    $output = '';
+
+    if ($posts) {
+		$output .= '<ul ' . get_block_wrapper_attributes() . '>';
+
+		foreach ($posts as $key => $post) {
+			$id = $post->ID;
+			$title = $post->post_title;
+			$excerpt = $post->post_excerpt;
+			$permalink = get_permalink($id);
+			$date = get_the_date('F j, Y', $id); // Get the post date
+			$thumbnail = get_the_post_thumbnail($id, 'full'); // Get the post thumbnail (image)
+		
+			$output .= '<li>';
+			$output .= '<a href="' . esc_url($permalink) . '">';
+			
+			if ($thumbnail && $display_image) {
+				$output .= '<div class="post-thumbnail">' . $thumbnail . '</div>'; // Display the thumbnail
+			}
+			
+			if ($title) {
+				$output .= '<h5>' . $title . '</h5>';
+			}
+			
+			if ($date) {
+				$output .= '<p class="post-date">' . $date . '</p>'; // Display the post date
+			}
+			
+			if ($excerpt) {
+				$output .= '<div class="post-excerpt">' . $excerpt . '</div>'; // Display the post excerpt
+			}
+			
+			$output .= '</a>';
+			$output .= '</li>';
+		}
+		
+		$output .= '</ul>';
+		
+    }
+
+    return $output;
+};
+
+function create_first_dynamic_gutenberg_block_block_init() {
+	register_block_type_from_metadata(__DIR__ . '/build', array(
+		'render_callback' => 'blocks_course_render_frontend'
+	));
 }
 add_action('init', 'create_first_dynamic_gutenberg_block_block_init');
--- src/save.js
+++ src/save.js
@@ -1,9 +1,3 @@
-import { useBlockProps } from '@wordpress/block-editor';
-
 export default function save() {
-	return (
-		<p {...useBlockProps.save()}>
-			{'Custom Test Block – hello from the saved content!'}
-		</p>
-	);
+	return null;
 }
