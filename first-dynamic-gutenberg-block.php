4d206881bdb6ad428bfa53be56e110ec3d7dc707
Alex Tonkins <alex.tonkins@nobleperforms.co.uk>
2023-08-04 15:28:28 +0100

    update naming conventions of each file

--- first-dynamic-gutenberg-block.php
+++ first-dynamic-gutenberg-block.php
@@ -1,14 +1,15 @@
 <?php
+
 /**
- * Plugin Name:       Custom Block Boilerplate
- * Description:       A block containing nothing but boilerplate. Use this as a starting point to create custom blocks.
+ * Plugin Name:       First Dynamic Gutenberg Block
+ * Description:       That sweet first dynamic block.
  * Requires at least: 6.1
  * Requires PHP:      7.0
  * Version:           1.0.0
  * Author:            Alex Tonkins
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain:       custom-block-boilerplate
+ * Text Domain:       first-dynamic-gutenberg-block
  *
  * @package           create-block
  */
@@ -20,7 +21,8 @@
  *
  * @see https://developer.wordpress.org/reference/functions/register_block_type/
  */
-function create_custom_block_boilerplate_block_init() {
-	register_block_type( __DIR__ . '/build' );
+function create_first_dynamic_gutenberg_block_block_init()
+{
+	register_block_type(__DIR__ . '/build');
 }
-add_action( 'init', 'create_custom_block_boilerplate_block_init' );
+add_action('init', 'create_first_dynamic_gutenberg_block_block_init');
--- src/edit.js
+++ src/edit.js
@@ -7,7 +7,7 @@ export default function Edit() {
 		<p {...useBlockProps()}>
 			{__(
 				'Custom Test Block – hello from the editor!',
-				'custom-block-boilerplate'
+				'first-dynamic-gutenberg-block'
 			)}
 		</p>
 	);
